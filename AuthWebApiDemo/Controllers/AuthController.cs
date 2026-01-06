d3c43d1df094a4ec65683f08b7e2cd0b1e64118d
Mayur Pakhale <Mayur.Pakhale@jet2.com>
2026-01-06 16:47:10 +0530

    Basic auth implementation

---
+++ AuthWebApiDemo/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthWebApiDemo.Entites;
+using AuthWebApiDemo.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User? user = new ();
+        private readonly IConfiguration configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User?> Register(UserDto request)
+        {
+            user!.Username = request.Username;
+            user.PasswordHash = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User?> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+                return BadRequest("User not found.");
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) 
+                == PasswordVerificationResult.Failed)
+                return BadRequest("Invalid password.");
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
---
+++ AuthWebApiDemo/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthWebApiDemo.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthWebApiDemo.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
